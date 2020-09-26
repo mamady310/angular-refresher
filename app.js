751db5d3065631426c6478439e73b47a1c0b427e
Mamady Sillah <supurpisces310@msn.com>
2020-09-25 21:18:53 -0400

    16 dependency injection and minification

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-var myApp = angular.module('myApp', ['ngMessages']);
+var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function($scope, $log, $filter) {
-    
-   $scope.name = 'John';
-   $scope.formattedname = $filter('uppercase')($scope.name);
-
-   $log.info($scope.name);
-   $log.info($scope.formattedname);
+myApp.controller('mainController', function($scope, $log) {
     
 });
-
-var searchPeople = function(firstName, $scope, height, age, occupation) {
-    return 'Jane Doe';
-}
-
-console.log(angular.injector().annotate(searchPeople));
---
+++ app15.js
@@ -0,0 +1,17 @@
+var myApp = angular.module('myApp', ['ngMessages']);
+
+myApp.controller('mainController', function($scope, $log, $filter) {
+    
+   $scope.name = 'John';
+   $scope.formattedname = $filter('uppercase')($scope.name);
+
+   $log.info($scope.name);
+   $log.info($scope.formattedname);
+    
+});
+
+var searchPeople = function(firstName, $scope, height, age, occupation) {
+    return 'Jane Doe';
+}
+
+console.log(angular.injector().annotate(searchPeople));
