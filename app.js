bd42ff2d12ef64beadbf011d54f6cfd9839264a3
Mamady Sillah <supurpisces310@msn.com>
2020-09-27 15:53:29 -0400

    ng-model directive

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$timeout', function($scope, $timeout) {
-    $scope.name = 'Mamady';
-
-    $timeout(function() {
-    $scope.name = 'Everybody';
-    }, 3000)
+myApp.controller('mainController', ['$scope', function($scope) {
+    
+    
     
 }]);
