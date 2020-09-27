8b7e06f9f57be348e7cb0840de04cb22a9c99981
Mamady Sillah <supurpisces310@msn.com>
2020-09-27 15:00:33 -0400

    scope and timeout utilities

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', function($scope) {
-    $scope.name = 'Tony';
+myApp.controller('mainController', ['$scope', '$timeout', function($scope, $timeout) {
+    $scope.name = 'Mamady';
+
+    $timeout(function() {
+    $scope.name = 'Everybody';
+    }, 3000)
     
 }]);
