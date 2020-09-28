2518ea19a2612793693d0fe0439ae8c475628ba6
Mamady Sillah <supurpisces310@msn.com>
2020-09-28 17:11:17 -0400

    multiple controllers and multiple views

--- app.js
+++ app.js
@@ -1,36 +1,7 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$filter','$http', function ($scope, $filter, $http) {
-
-    $scope.handle = '';
-
-    $scope.lowercasehandle = function () {
-        return $filter('lowercase')($scope.handle);
-    };
-
-    $scope.characters = 5;
-
-    // get request
-    $http.get('https://api.openbrewerydb.org/breweries?by_state=new_york')
-       .success(function(result) {
-        $scope.name = result;
-        // console.log(result);
-       })
-       .error(function(data, status) {
-           console.log(data);
-       })
-
-       // start of post request
-       $scope.newRule = '';
-       $scope.addRule = function (){
-           $http.post('url', {newRule: $scope.newRule})
-           .success(function(result) {
-            $scope.name = result;
-            $scope.newRule = '';
-           })
-           .error(function(data, status) {
-               console.log(data);
-           })
-       };
-
+myApp.controller('mainController', ['$scope', function($scope) {
+    
+    
+    
 }]);
