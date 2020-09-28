0cd43a092642e66a41758b96c325f42dc4a9f436
Mamady Sillah <supurpisces310@msn.com>
2020-09-28 19:34:51 -0400

    created function to create paths to different pages

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', function($scope) {
-    
-
+myApp.controller('mainController', ['$scope','$location','$log', function($scope, $location, $log) {
+    function($scope, $location, $log) {
+        $log.info($location.path());
+    }
     
 }]);
