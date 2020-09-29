b4bb6e0046d3284e73325706701a6eb700921a15
Mamady Sillah <supurpisces310@msn.com>
2020-09-28 20:25:46 -0400

    pasted ngroute cdn and added ngRoute service into

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function($routeProvider) {
+
+});
 
 myApp.controller('mainController', ['$scope','$log', function($scope,$log) {
 
