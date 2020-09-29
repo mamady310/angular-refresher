b25298f073b7021f958b019febfa83c212efa028
Mamady Sillah <supurpisces310@msn.com>
2020-09-28 20:28:22 -0400

    create routes for maincontroller and second controller

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(function($routeProvider) {
 
+    $routeProvider
+
+    .when('/', {
+        //connects the template to controller
+        templateUrl: 'pages/main.html',
+        controller: 'mainController'
+    })
+    .when('/second', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+
+    })
 });
 
 myApp.controller('mainController', ['$scope','$log', function($scope,$log) {
